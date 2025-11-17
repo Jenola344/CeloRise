3bf6b505b6f55687943dc4790e02c4d24f47da79
JESUTOLA OLUSEGUN <jesutolaolusegun@gmail.com>
2025-11-17 18:25:02 +0000

    for the walletconnect wallect SDK for wallet connection, allow to use Wa

--- src/components/Web3ModalProvider.tsx
+++ src/components/Web3ModalProvider.tsx
@@ -34,7 +34,13 @@ createWeb3Modal({
   wagmiConfig: config,
   projectId,
   metadata,
-  enableAnalytics: true // Optional - defaults to your Cloud configuration
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  featuredWalletIds: [
+    'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96', // MetaMask
+    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0', // Trust Wallet
+    'f2436c67184f158d1beda5df53298ee84abfc367581e4505134b5bcf5f46697d', // WalletConnect (Reown)
+    '1ae92b26df02f0abca6304df07deb48179f9940a63ea91c3345d28e205421df7', // Rainbow
+  ]
 })
 
 export function Web3ModalProvider({ children }: { children: ReactNode }) {
